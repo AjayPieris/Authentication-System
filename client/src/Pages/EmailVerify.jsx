81a8defb6fc671e28907831093e2496b4d51a185
AjayPieris <ajayperies10@gmail.com>
2025-09-18 11:13:14 +0530

    Email verify with react

--- client/src/Pages/EmailVerify.jsx
+++ client/src/Pages/EmailVerify.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React,{use, useContext} from 'react'
 import { useNavigate } from 'react-router-dom'
 import { assets } from '../assets/assets'
+import axios from 'axios';
+import { AppContent } from '../context/AppContext';
+import { toast } from 'react-toastify';
 
 function EmailVerify() {
 
-  const inputRefs = React.useRef([]);
+  axios.defaults.withCredentials = true;
+
+  const {backendUrl, isLoggedIn, userData, getuserData} = useContext(AppContent);
+  const inputRefs = React.useRef([]); 
+
+  
   const navigate = useNavigate();
 
   const handleInput = (e, index) => {
@@ -28,6 +36,42 @@ function EmailVerify() {
   }
 }
 
+const handlePaste = (e) => {
+  e.preventDefault();
+  const paste = e.clipboardData.getData('text');
+  const pasteArray = paste.split('').slice(0, 6);
+  pasteArray.forEach((char, index) => {
+    if (inputRefs.current[index]) {
+      inputRefs.current[index].value = char;
+    }
+  });
+};
+
+const onSubmitHandler = async(e) => {
+ try{
+  e.preventDefault();
+  const otpArray = inputRefs.current.map(input => input.value);
+  const otp = otpArray.join('');
+  console.log('Submitted OTP:', otp);
+  const{data} = await axios.post(backendUrl+'/api/auth/verify-account',{otp});
+  // Add your OTP submission logic here
+  if(data.success){
+    toast.success(data.message);
+    getuserData();
+    navigate('/');
+  }else{
+    toast.error(data.message);
+  }
+ }
+ catch(err){
+  console.error('Error submitting OTP:', err);
+ }
+};
+
+useEffect(() => {
+  isLoggedIn && userData && userData.isVerified && navigate('/');
+}, [isLoggedIn, userData]);
+
   return (
     <div className="flex items-center justify-center min-h-screen px-6 sm:px-0 bg-gradient-to-br from-blue-200 to-purple-400">
       <img
@@ -36,10 +80,10 @@ function EmailVerify() {
                 onClick={() => navigate("/")}
                 className="absolute left-5 sm:left-20 top-5 w-28 sm:w-32 cursor-pointer"
               />
-        <form className='bg-slate-900 p-8 rounded-lg shadow-lg w-96 text-sm'>
+        <form onSubmit={onSubmitHandler} className='bg-slate-900 p-8 rounded-lg shadow-lg w-96 text-sm'>
             <h1 className='text-white text-2xl font-semibold text-center mb-4'>Email Verify Otp</h1>
             <p className='text-center mb-6 text-indigo-300'>Enter the 6-digit code sent your email id.</p>
-            <div className='flex justify-between mb-8'>
+            <div className='flex justify-between mb-8' onPaste={handlePaste}>
               {Array(6).fill(0).map((_, index) => (
                 <input 
                   key={index}
